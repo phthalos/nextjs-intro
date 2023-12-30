225deee3b558809a7cc219fabe3769c037e5901c
phthalos <phthalos64@gmail.com>
2023-12-30 23:08:01 +0900

    2.2 Redirect and Rewrite

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const API_KEY = process.env.API_KEY;
+
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    async redirects() {
+        return [
+            {
+                source: "/contact/:path*",
+                destination: "/form/:path*",
+                permanent: false,
+            },
+        ];
+    },
+    async rewrites() {
+        return [
+            {
+                source: "/api/movies",
+                destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+            },
+        ];
+    },
+};
+
+module.exports = nextConfig;
---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import NavBar from "../components/NavBar";
+import Seo from "../components/Seo";
+
+export default function About() {
+    return (
+        <div>
+            <Seo title="About" />
+            <h1>about</h1>
+        </div>
+    );
+}
